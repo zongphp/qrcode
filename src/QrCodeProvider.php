dc3eba1b0af583da0f13c66fd5925dcf7db2b313
zmingzhi <zmingzhi@yeah.net>
2018-01-11 17:28:07 +0800

    2.0

--- src/QrCodeProvider.php
+++ src/QrCodeProvider.php
@@ -1,15 +1,21 @@
 <?php
+
 namespace zongphp\qrcode;
 
 use zongphp\framework\build\Provider;
 
-class QrCodeProvider extends Provider {
-	//延迟加载
-	public $defer = true;
+class QrCodeProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	public function register() {
-		$this->app->single( 'QrCode', function () {
-			return QrCode::single();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'QrCode',
+            function () {
+                return QrCode::single();
+            }
+        );
+    }
 }
